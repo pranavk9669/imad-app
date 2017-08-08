64cc7b9d17b8220c756754f7c8cfe80670a96108
pranavk9669 <30757969+pranavk9669@users.noreply.github.com>
2017-08-08 23:05:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one', function (req, res) {
+  res.send('son of a gun');
+});
+
+app.get('/article-two', function (req, res) {
+  res.send('hel two');
+});
+
+app.get('/article-three', function (req, res) {
+  res.send('kill the batman');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
